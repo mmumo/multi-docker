0e14e9b7c8ac1216243c4eb5d322ca4e0a7d00b8
mmumo <markmumo@gmail.com>
2023-11-27 22:59:17 +0300

    Initial Commit7

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,10 +3,3 @@ import App from './App';
 
 test('renders learn react link', () => {
 });
-
-test2('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-}
-);
